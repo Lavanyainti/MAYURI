bae960cdf8cf98646519973f00a0fc30b0ae6266
Lavanyainti <lavanyainti14@gmail.com>
2026-04-09 16:21:26 +0530

    First commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const projectRouter = require('./routes/projectRoute');
 const AuthRouter=require('./routes/authRouter')
 
 app.use(cors({
-    origin: ["https://gilded-madeleine-34ffb8.netlify.app"], 
+    origin: ["https://adorable-dasik-85b080.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
