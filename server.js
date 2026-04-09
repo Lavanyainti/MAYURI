f7809370a44ddbc954431c6aa0aa35ceebec73b3
Lavanyainti <lavanyainti14@gmail.com>
2026-04-09 16:05:59 +0530

    third commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const projectRouter = require('./routes/projectRoute');
 const AuthRouter=require('./routes/authRouter')
 
 app.use(cors({
-    origin: ["https://mellow-kashata-f83f59.netlify.app"], 
+    origin: ["https://gilded-madeleine-34ffb8.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
