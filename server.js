9e4821e639ec79852d6457568e04947a719cc485
Lavanyainti <lavanyainti14@gmail.com>
2026-04-15 19:42:49 +0530

    fifth commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const projectRouter = require('./routes/projectRoute');
 const AuthRouter=require('./routes/authRouter')
 
 app.use(cors({
-    origin: ["https://super-medovik-eff945.netlify.app"], 
+    origin: ["https://benevolent-scone-cd92a6.netlify.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
